00421ab2f35892c81582e9c7300baf3dcdd88682
mehulbhuradia <mehulbhuradia007@gmail.com>
2022-04-28 16:42:05 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nftweb3app/',
+  base: '/web3_nft_minter/',
   plugins: [react()]
 })
 
